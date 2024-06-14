e91a1755a1870a2a93a73407a12d57a7e8380129
Khanh Nguyen <khanh@propolife.co.jp>
2024-06-14 13:35:16 +0700

    Passing data to Pages

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,20 @@
 <template>
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
+    <div>
+        The message is "{{message}}"
+    </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+
+/* Pass params from Laravel to Vue's props */
+// Method 1:
+// defineProps(['message']);
+
+// Method 2: With validation
+defineProps({
+    message: String
+});
 </script>
