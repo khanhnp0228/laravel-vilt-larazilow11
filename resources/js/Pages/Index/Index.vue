53d10b58d65946f0b5a3db06821521116dc2f95f
Khanh Nguyen <khanh@propolife.co.jp>
2024-06-14 14:13:55 +0700

    Layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,19 +1,17 @@
 <template>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
-    <div>
-        The message is "{{message}}"
-    </div>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>
+            The message is {{ message }}
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import MainLayout from '../../Layouts/MainLayout.vue';
 
-/* Pass params from Laravel to Vue's props */
-// Method 1:
-// defineProps(['message']);
-
-// Method 2: With validation
 defineProps({
     message: String
 });
