9038a0751d13f0948f292fdc749f401d27ffb07f
Khanh Nguyen <khanh@propolife.co.jp>
2024-06-18 13:00:05 +0700

    Sharing Data with All Pages with usePage

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,13 +3,28 @@
     <Link href="/hello">Show Page</Link>
     <div>The page with time {{ timer }}</div>
 
+    <div v-if="page.props.flash.success" class="success">
+        {{ page.props.flash.success }}
+    </div>
+
     <slot></slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 const timer = ref(0);
 setInterval(() => timer.value++, 1000);
+
+// page.props.flash.success
+const page = usePage()
+
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: #fff;
+}
+</style>
