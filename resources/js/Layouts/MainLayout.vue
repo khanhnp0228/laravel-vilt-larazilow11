bf4338bc27e7278325ee9ca84ef65f9383650533
Khanh Nguyen <khanh@propolife.co.jp>
2024-06-18 15:23:47 +0700

    Edit Form

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,8 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>
+    <Link href="/hello">Show Page</Link>&nbsp;
+    <Link href="/listing">Listing</Link>&nbsp;
+    <Link href="/listing/create">New Listing</Link>&nbsp;
     <div>The page with time {{ timer }}</div>
 
     <div v-if="flashSuccess" class="success">
